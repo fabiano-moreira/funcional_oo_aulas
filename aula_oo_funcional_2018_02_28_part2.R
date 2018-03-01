4be64781f2897515b38a842f592082db62943e55
Fabiano Moreira <fabiano.moreira.alves@gmail.com>
2018-02-28 22:30:50 -0300

    Upload aula_oo_funcional_2018_02_28_part2

---
+++ aula_oo_funcional_2018_02_28_part2.R
@@ -0,0 +1,28 @@
+# Hands on
+
+data("iris")
+View(iris)
+
+help("sqrt") #O help das funções
+
+help("iris")
+data
+
+#missing data
+
+missing(idades)
+
+idades <- c(20, 22, NA)
+
+idades
+
+class(idades)
+
+
+dim(iris) #tamanho do banco de dados
+names(iris) #nomes das variáveis do banco de dados
+
+sapply(iris, class)
+
+str(iris) #Estrutura do banco de dados
+
